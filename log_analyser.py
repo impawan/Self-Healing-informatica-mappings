8603d6f691b117f72c061556e04cfa44b44a3753
PawanPrasad <pawanprasad@outlook.com>
2019-07-22 09:03:54 +0530

    done

--- log_analyser.py
+++ log_analyser.py
@@ -9,7 +9,8 @@ Created on Thu Jul 18 12:35:07 2019
 
 import numpy as np
 import pandas as pd
-
+from xml.dom import minidom
+from lxml import etree
 
 error_matrix = list()
 
@@ -19,10 +20,25 @@ log = log.read()
 log = log.split('\n')
 for row in log:
     if ': ERROR :' in row:
-        print(row) 
         error_matrix.append(row.split(':'))
 
 
 
 df = pd.DataFrame(error_matrix)
-print(df)
+
+mapping_xml  = open('m_CrDbStatsment.xml','r')
+
+mapping_xml = mapping_xml.read()
+
+
+def modify_xml(mapping_xml):
+#    doc = minidom.parse("m_CrDbStatsment.xml")
+#    name = doc.getElementsByTagName("CONNECTOR")[0]
+#    print(name.nodeName)
+    root = etree.fromstring(mapping_xml)
+    etree.tostring(root)
+    
+    
+    
+    
+modify_xml(mapping_xml)
