3fca9f26cebfb168b8639f120b0a1e3bf1ca8423
PawanPrasad <pawanprasad@outlook.com>
2019-07-24 15:49:57 +0530

    Added powrmart.dtd for xml validation, now fixed xml is generated add base XML for future refernce

--- log_analyser.py
+++ log_analyser.py
@@ -9,14 +9,19 @@ Created on Thu Jul 18 12:35:07 2019
 
 import numpy as np
 import pandas as pd
-from lxml import etree
+from lxml import etree,objectify
 from io import StringIO, BytesIO
 import re
 
 
 
+XML_metaData = '''<?xml version="1.0" encoding="Windows-1252"?>
+<!DOCTYPE POWERMART SYSTEM "powrmart.dtd">
+<POWERMART CREATION_DATE="07/23/2019 12:36:42" REPOSITORY_VERSION="184.93">
+<REPOSITORY NAME="INFA_REP" VERSION="184" CODEPAGE="MS1252" DATABASETYPE="Oracle">'''
 
-
+XML_end = '''</REPOSITORY>
+</POWERMART>'''
 error_matrix = list()
 
 log = open('logs.txt', 'r')
@@ -35,7 +40,20 @@ df = pd.DataFrame(error_matrix)
 file = 'm_CrDbStatsment_orginal.xml'
 
            
-                            
+def validate(xmlschema_doc) -> bool:
+    dtd_file = open("C:/Informatica/9.6.1/server/bin/powrmart.dtd",'rb')
+    dtd_file = dtd_file.read()
+    
+    dtd = etree.DTD(dtd_file)
+    tree = objectify.parse(xmlschema_doc)
+    print (dtd.validate(tree))
+    xmlschema = etree.XMLSchema(StringIO(xmlschema_doc))
+
+  
+    result = xmlschema.validate(xmlschema)
+
+    return result
+             
                
 ##
 def xml_formation(file):
@@ -53,16 +71,15 @@ mapping_xml = xml_formation(file)
 def fix_data_type(mapping_xml,port_name, data_type):
     XMLtree = etree.parse(StringIO(mapping_xml))
     node_list = list()
-    #LList = LinkedList()
     
-    #aandeel = tree.xpath('//a[@title="%s"]/text()' % var)
     node = XMLtree.xpath('//CONNECTOR[@FROMFIELD="%s"]'% port_name)[0]
     while(node is not None):
         FROMINSTANCE = node.xpath('./@FROMINSTANCE')[0]
         FROMINSTANCETYPE = node.xpath('./@FROMINSTANCETYPE')[0]
+        FROMFIELD = node.xpath('./@FROMFIELD')[0]
         TOFIELD = node.xpath('./@TOFIELD')[0]
         TOINSTANCE = node.xpath('./@TOINSTANCE')[0]
-        lnode = [FROMINSTANCE,FROMINSTANCETYPE,TOFIELD,TOINSTANCE]
+        lnode = [FROMINSTANCE,FROMINSTANCETYPE,FROMFIELD,TOFIELD,TOINSTANCE]
         node_list.append(lnode)
         new_xpath = '//CONNECTOR[@FROMFIELD=\''+TOFIELD+'\'][@FROMINSTANCE=\''+TOINSTANCE+'\']'
         node = XMLtree.xpath(new_xpath)
@@ -80,17 +97,20 @@ def fix_data_type(mapping_xml,port_name, data_type):
             node = XMLtree.xpath('//SOURCE[@NAME="%s"]' % i[0])[0]
             node = node.xpath('./SOURCEFIELD[@NAME="%s"]' % i[2])[0]
             node.set('DATATYPE' ,'number')
-            print(etree.tostring(XMLtree,pretty_print=True))
-#    for node in XMLtree.xpath('//CONNECTOR[@FROMFIELD=\'Branch_code\']'):
-#        instance_name = node.xpath('./@FROMINSTANCE')[0]
-#        instance_type = node.xpath('./@FROMINSTANCETYPE')[0]
-#        node = [instance_name,instance_type]
-#        node_list.append(node)
-#        LList.insertEnd(node)
-    #print(node_list)
-    #LList.printList()
-     
+            
+            
+        else:
+            
+            node = XMLtree.xpath('//TRANSFORMATION[@NAME="%s"]' % i[0])[0]
+            node = node.xpath('./TRANSFORMFIELD[@NAME="%s"]' % i[2])[0]
+            node.set('DATATYPE' ,'decimal')
         
+    fixed_XML =  etree.tostring(XMLtree,pretty_print=True).decode("utf-8") 
+    fixed_XML = XML_metaData+fixed_XML+XML_end
+    writeXML = open("fixed.xml","w")
+    writeXML.write(fixed_XML)
+    writeXML.close()
+
         
         
 fix_data_type(mapping_xml,'Branch_code','ff')    
